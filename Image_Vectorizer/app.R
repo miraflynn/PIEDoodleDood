a944f6902c624d1e3f33d3d9a883da9f437f9622
Mira Flynn <miraflynn0@gmail.com>
2021-11-01 22:33:16 -0400

    Add shiny app to vectorizer

--- Image_Vectorizer/app.R
+++ Image_Vectorizer/app.R
@@ -8,41 +8,155 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(magick)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Vectorize Image for Drawing Robot"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            fileInput(
+                "upload",
+                "Upload Image To Draw",
+                accept = "image/*"
+            ),
+            sliderInput("radius",
+                        "Radius:",
+                        min = 0,
+                        max = 20,
+                        value = 0),
+            
+            sliderInput("sigma",
+                        "Sigma:",
+                        min = 0,
+                        max = 20,
+                        value = 1),
+            
+            sliderInput("bounds_pct",
+                        "Lower and Upper Percent:",
+                        min = 0,
+                        max = 100,
+                        value = c(10,30))
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            imageOutput("edge_image",inline = TRUE),
+            br(),
+            imageOutput("original_image",inline = TRUE)
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    toListen <- reactive({
+        list(input$radius,input$sigma,input$bounds_pct,input$upload)
     })
+    
+    # radius = 0
+    # sigma = 1
+    # lower_pct = 10
+    # upper_pct = 30
+    # 
+    # geom_string <- paste(
+    #     radius,
+    #     "x",
+    #     sigma,
+    #     "+",
+    #     lower_pct,
+    #     "%+",
+    #     upper_pct,
+    #     ""
+    # )
+    # 
+    # processed_image <- image_read("pixar.jpg") %>%
+    #     image_canny(geometry = geom_string) %>%
+    #     image_write(tempfile(fileext='jpg'), format = 'jpg')
+    
+    observeEvent(
+        toListen(), {
+            
+            image_path <- ifelse(
+                length(input$upload$datapath),
+                as.character(input$upload$datapath),
+                "olin.jpg"
+            )
+
+            uploaded_image <- image_read(image_path) %>%
+            image_write(tempfile(fileext='jpg'), format = 'jpg')
+            
+            output$original_image <- renderImage({
+                
+                # filename <-  file.path(
+                #     './images', 
+                #     paste('image', input$n, '.jpg', sep = '')) %>%
+                #     normalizePath()
+                filename <-  uploaded_image
+                
+                # Return a list containing the filename and alt text
+                list(
+                    src = filename,
+                    alt = paste("Original Image"),
+                    width = "70%",
+                    height = "auto",
+                    inline = TRUE
+                )
+                
+                
+            }, deleteFile = FALSE)
+            
+            
+            radius = input$radius
+            sigma = input$sigma
+            lower_pct = input$bounds_pct[1]
+            upper_pct = input$bounds_pct[2]
+            
+            geom_string <- paste(
+                radius,
+                "x",
+                sigma,
+                "+",
+                lower_pct,
+                "%+",
+                upper_pct,
+                ""
+            )
+            
+            processed_image <- image_read(image_path) %>%
+                image_canny(geometry = geom_string) %>%
+                image_write(tempfile(fileext='jpg'), format = 'jpg')
+            
+            output$edge_image <- renderImage({
+                
+                # filename <-  file.path(
+                #     './images', 
+                #     paste('image', input$n, '.jpg', sep = '')) %>%
+                #     normalizePath()
+                filename <-  file.path(
+                    processed_image
+                ) %>%
+                    normalizePath()
+                
+                # Return a list containing the filename and alt text
+                list(
+                    src = filename,
+                    alt = paste("Original Image"),
+                    width = "70%",
+                    height = "auto",
+                    inline = TRUE
+                )
+                
+                
+            }, deleteFile = FALSE)
+        }
+    )
 }
 
 # Run the application 
