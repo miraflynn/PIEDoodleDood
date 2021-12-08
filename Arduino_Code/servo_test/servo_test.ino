0f7ab2ec11163a41e2b35830f894df7c2e54116c
Mira Flynn <miraflynn0@gmail.com>
2021-12-08 13:12:36 -0500

    Slightly rewriting some parts of servo_test to work better with drive code when copied

--- Arduino_Code/servo_test/servo_test.ino
+++ Arduino_Code/servo_test/servo_test.ino
@@ -10,27 +10,39 @@
 #include <Servo.h>
 
 int downPos = 22;
+int upPos = downPos + 30;
 
-Servo myservo;  // create servo object to control a servo
+Servo penServo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
-
-void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+void penUp(){
+  penServo.write(upPos);
+  delay(200);
 }
 
+void penDown(){
+  penServo.write(downPos);
+  delay(200);
+}
+
+void setup() {
+  penServo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+//int pos = 0;    // variable to store the servo position
 void loop() {
-  for (pos = downPos; pos <= downPos + 30; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  delay(2000);
-  for (pos = downPos + 30; pos >= downPos; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  delay(2000);
+//  for (pos = downPos; pos <= downPos + 30; pos += 1) { // goes from 0 degrees to 180 degrees
+//    // in steps of 1 degree
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(15);                       // waits 15ms for the servo to reach the position
+//  }
+//  delay(2000);
+//  for (pos = downPos + 30; pos >= downPos; pos -= 1) { // goes from 180 degrees to 0 degrees
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(15);                       // waits 15ms for the servo to reach the position
+//  }
+//  delay(2000);
 //  myservo.write(25);
+  penUp();
+  penDown();
 }
